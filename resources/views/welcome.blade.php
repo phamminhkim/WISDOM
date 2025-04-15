cf2ef11fcc0064fb7cb0a3645cb92c70d08d5311
phamminhkim <kienpham.100501@gmail.com>
2025-04-15 19:29:04 +0700

    Test mới

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel & Vue.js</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+<script src="{{ mix('js/app.js') }}"></script>
 </head>
 <body>
     <div id="app">
